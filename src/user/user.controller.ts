2fc7ec1c50e8da111decaf29831c233f8d04aa73
REGENS_LAPTOP\regen <regens.global@gmail.com>
2024-11-11 20:05:33 +0200

    user deleting realized

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Controller,Body ,Post,Get } from '@nestjs/common';
+import { Controller, Delete} from '@nestjs/common';
 import { UserService } from "./user.service";
+import { CurrentUserId } from "../decorators/current-user.decorator";
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService ) {}
 
+  @Delete('delete')
+  deleteUser(@CurrentUserId() userId: number) {
+    return this.userService.deleteUser(userId);
+  }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,25 +7,12 @@ export class UserService {
   constructor(private prisma: PrismaService) {
   }
 
-  async registerUser(name: string, surname: string, email: string, password: string) {
-    return this.prisma.user.create({
-      data: {
-        name,
-        surname,
-        email,
-        password,
-        isActive: true,
+  async deleteUser(userId: number) {
+    return this.prisma.user.delete({
+      where: {
+        id: userId
       }
     });
   }
 
-  async userAuth(email: string,password: string): Promise<number> {
-    const user = await this.prisma.user.findFirst({
-      where: {
-        email: email,
-        password: password
-      }
-    });
-    return  user.id;
-  }
 }
